35537eb7fc76668c7353f3320057941b35c73ad4
Joshua Michaels <joshuaiz@me.com>
2023-11-05 19:24:13 -0600

    adds vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
+import vercel from '@astrojs/vercel/serverless'
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [
@@ -8,4 +10,10 @@ export default defineConfig({
             applyBaseStyles: false,
         }),
     ],
+    output: 'server',
+    adapter: vercel(),
+    server: {
+        port: 7723,
+        host: true,
+    },
 })
