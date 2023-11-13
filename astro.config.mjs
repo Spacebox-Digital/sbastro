e30c5b1369abd2c29731699e8d0959768a61598b
Joshua Michaels <joshuaiz@me.com>
2023-11-12 21:34:31 -0600

    updates deps; adds satellite (cool)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  })],
-  output: 'server',
-  adapter: vercel(),
-  server: {
-    port: 7723,
-    host: true
-  }
-});
+	integrations: [
+		tailwind({
+			applyBaseStyles: false
+		})
+	],
+	output: 'server',
+	adapter: vercel(),
+	server: {
+		port: 7723,
+		host: true
+	}
+})
