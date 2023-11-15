b5371440fb1585b9dc4cc997637c1cb0c9dbcd06
Joshua Michaels <joshuaiz@me.com>
2023-11-14 23:42:23 -0600

    updates what we do

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind({
-			applyBaseStyles: false
-		})
-	],
-	output: 'server',
-	adapter: vercel(),
-	server: {
-		port: 7723,
-		host: true
-	}
-})
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), mdx()],
+  output: 'server',
+  adapter: vercel(),
+  server: {
+    port: 7723,
+    host: true
+  }
+});
